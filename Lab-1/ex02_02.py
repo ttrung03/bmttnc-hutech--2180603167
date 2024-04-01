58f4928f9eeae861aac66750768e949aa54ca0e1
Vo Thanh Trung <Vothanhtrung9379@gmail.com>
2024-04-01 18:16:41 +0700

    [add] lab-1 ex01 ex02

---
+++ Lab-1/ex02_02.py
@@ -0,0 +1,4 @@
+ban_kinh = float(input("Nhập bán kính của hình tròn: "))
+dien_tich = 3.14 * (ban_kinh ** 2)
+# in ra màn hình
+print("Diện tích của hình tròn là:", dien_tich)
---
+++ Lab-1/ex02_03.py
@@ -0,0 +1,6 @@
+so = int(input("Nhập 1 số nguyên: "))
+# kiểm tra chẵn lẻ
+if so % 2 == 0:
+    print(so, "Là số chẵn!")
+else:
+    print(so, "Không phải là số chẵn.")
